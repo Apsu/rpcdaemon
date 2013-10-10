65fa05100125f4bc50835aafb0345d4b4be921a7
Evan Callicoat <diopter@gmail.com>
2013-10-10 01:33:49 -0500

    Mebbe better?

--- rpcdaemon/lib/plugin.py
+++ rpcdaemon/lib/plugin.py
@@ -34,23 +34,23 @@ class Plugin(RPC):
             self,
             connection,
             exopts={
-                'name': self.config['exchange_name']
-                if 'exchange_name' in self.config else self.sublower,
-                'durable': self.config['exchange_durable']
-                if 'exchange_durable' in self.config else False,
-                'type': self.config['exchange_type']
-                if 'exchange_type' in self.config else 'topic'
+                'name': (self.config['exchange_name']
+                if 'exchange_name' in self.config else None),
+                'durable': (self.config['exchange_durable']
+                if 'exchange_durable' in self.config else False),
+                'type': (self.config['exchange_type']
+                if 'exchange_type' in self.config else 'topic')
             },
             qopts={
-                'name': self.config['queue_name']
+                'name': (self.config['queue_name']
                 if 'queue_name' in self.config else
-                'rpcdaemon-%s_%s' % (self.sublower, uuid4()),
-                'auto_delete': self.config['queue_auto_delete']
-                if 'queue_auto_delete' in self.config else True,
-                'durable': self.config['queue_durable']
-                if 'queue_durable' in self.config else False,
-                'routing_key': self.config['queue_routing_key']
+                'rpcdaemon-%s_%s' % (self.sublower, uuid4())),
+                'auto_delete': (self.config['queue_auto_delete']
+                if 'queue_auto_delete' in self.config else True),
+                'durable': (self.config['queue_durable']
+                if 'queue_durable' in self.config else False),
+                'routing_key': (self.config['queue_routing_key']
                 if 'queue_routing_key' in self.config else
-                'glance_notifications.info'
+                'glance_notifications.info')
             }
         )
