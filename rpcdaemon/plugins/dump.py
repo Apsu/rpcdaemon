30195fd0eedc7c63826525938a06498315868667
rpedde <ron@pedde.com>
2013-11-06 21:56:14 +0000

    message format fix as with neutronagent

--- rpcdaemon/plugins/dump.py
+++ rpcdaemon/plugins/dump.py
@@ -46,5 +46,8 @@ class Dump(RPC):
         pass
 
     def update(self, body, message):
+        if 'oslo.message' in body:
+            body = json.loads(body['oslo.message'])
+
         self.logger.debug(json.dumps(body, indent=2, sort_keys=True))
         message.ack()
